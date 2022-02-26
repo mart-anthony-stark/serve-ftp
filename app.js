1eb38c37d5cccad4aa077b28ab5ae1893e87d4ab
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-26 21:20:30 +0800

    add: serve index middleware

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const serveIndex = require("serve-index");
 
 const app = express();
+
+app.use(
+  "/ftp",
+  express.static("public"),
+  serveIndex("public", { icons: true })
+);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
